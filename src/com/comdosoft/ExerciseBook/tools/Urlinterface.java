02bfd612aca5dc32429676dd5b3f4ad4dfc41188
unknown <305845671@qq.com>
2014-02-20 12:29:05 +0800

    update detial

---
+++ src/com/comdosoft/ExerciseBook/tools/Urlinterface.java
@@ -0,0 +1,13 @@
+package com.comdosoft.ExerciseBook.tools;
+
+public interface Urlinterface {
+	static final String tag = "ExerciseBook";
+	static final String SHARED = "EB";
+
+//	 static final String IP = "http://192.168.0.250:3004";
+
+	 static final String IP = "http://116.255.202.123:3011";
+	// static final String IP = "http://192.168.0.101:3004";
+	// static final String IP = "http://192.168.0.127:3000";
+
+}
