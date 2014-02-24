027977013df1bd02162814cc2abd1807adb1d226
unknown <305845671@qq.com>
2014-02-24 10:00:10 +0800

    zxn

---
+++ src/com/comdosoft/ExerciseBook/pojo/QuestionPojo.java
@@ -0,0 +1,53 @@
+package com.comdosoft.ExerciseBook.pojo;
+
+import java.util.List;
+
+public class QuestionPojo {
+
+	private int id;
+	private String content;
+	private String url;
+
+	public QuestionPojo() {
+		super();
+	}
+	
+
+	public QuestionPojo(int id, String content, String url) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.url = url;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return "QuestionPojo [id=" + id + ", content=" + content + ", url="
+				+ url + "]";
+	}
+
+}
